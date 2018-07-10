5c798c5e4d438ddc2c0e9c74e69b3f7370ff03f3
jijothic <george.jijothic@gmail.com>
2018-07-10 15:09:41 +0300

    setup graphql server and added faker to mock data

--- server/src/index.js
+++ server/src/index.js
@@ -1,5 +1,38 @@
 import express from 'express';
+import { graphqlExpress, graphiqlExpress } from 'apollo-server-express';
+import { makeExecutableSchema } from 'graphql-tools';
+import { createServer } from 'http';
 import bodyParser from 'body-parser';
+
+import './config/db';
+import constants from './config/constants';
+import typeDefs from './graphql/schema';
+import resolvers from './graphql/resolvers';
+
+app.use(bodyParser.json());
+
+app.use(
+  '/graphiql',
+  graphiqlExpress({
+    endpointURL: constants.GRAPHQL_PATH,
+  }),
+);
+
+app.use(
+  constants.GRAPHQL_PATH,
+  graphiqlExpress({
+    schema
+  }),
+);
+
+graphqlServer.listen(constants.PORT, err =>{
+  if (err) {
+    console.error(err);
+  }else{
+    console.log(`App running on port: ${constants.PORT}`);
+  }
+});
+
 
 const app = express(); // create an instance of express
 
